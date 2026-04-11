00aec02c7a8e15ee193c1ab71c63a9c40b088fdc
Shubh Jaiswal <sj75818378908.com>
2026-04-12 03:57:16 +0530

    Fix Vercel deployment and hardcode database connection for HR submission

--- testmybrain/settings.py
+++ testmybrain/settings.py
@@ -56,14 +56,13 @@ WSGI_APPLICATION = 'testmybrain.wsgi.application'
 
 import dj_database_url
 
-DATABASE_URL = os.environ.get('DATABASE_URL')
+DATABASE_URL = 'postgresql://postgres.uhvafwnceynvcvhdiapl:%21%21T3%3FeZpdDVCrhZ@aws-1-ap-southeast-2.pooler.supabase.com:6543/postgres'
 
 if DATABASE_URL:
     DATABASES = {
-        'default': dj_database_url.config(
-            default=DATABASE_URL,
+        'default': dj_database_url.parse(
+            DATABASE_URL,
             conn_max_age=600,
-            conn_health_checks=True,
         )
     }
 else:
